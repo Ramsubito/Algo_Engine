3ad13be499a2c16a5b27a1034ff8473a776de54f
Marc Pavon Llop <60131792+Marckitus@users.noreply.github.com>
2021-09-21 21:08:56 +0200

    Added imgui
    added imgui with 2 funcional windows (we have to clean the imgui folder because it has the entire imgui github download with lots of unused files)

--- Algo_Engine/ModuleSceneIntro.cpp
+++ Algo_Engine/ModuleSceneIntro.cpp
@@ -18,6 +18,14 @@ bool ModuleSceneIntro::Start()
 
 	App->camera->Move(vec3(1.0f, 1.0f, 0.0f));
 	App->camera->LookAt(vec3(0, 0, 0));
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+
+	ImGui::StyleColorsDark();
+	ImGui_ImplSDL2_InitForOpenGL(App->window->window, App->renderer3D->context);
+	ImGui_ImplOpenGL3_Init();
+
 	return ret;
 }
 
@@ -35,6 +43,43 @@ update_status ModuleSceneIntro::Update(float dt)
 	Plane p(0, 1, 0, 0);
 	p.axis = true;
 	p.Render();
+
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui_ImplSDL2_NewFrame();
+	ImGui::NewFrame();
+
+	{
+		static float f = 0.0f;
+
+		ImGui::Begin("Close me to close the App!");                          // Create a window called "Hello, world!" and append into it.
+
+		if (ImGui::Button("Exit"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+			return UPDATE_STOP;
+
+		ImGui::End();
+	}
+	{
+		static float f = 0.0f;
+
+		ImGui::Checkbox("Demo Window", &show_demo_window);
+		if (show_demo_window)
+			ImGui::ShowDemoWindow(&show_demo_window);
+			
+	}
+
+	//if (show_another_window)
+	//{
+	//    ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+	//    ImGui::Text("Hello from another window!");
+	//    if (ImGui::Button("Close Me"))
+	//        show_another_window = false;
+	//    ImGui::End();
+	//}
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	ImGui::Render();
+	glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+	glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
 	return UPDATE_CONTINUE;
 }
